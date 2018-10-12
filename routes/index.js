326d356b596082d3f58472b4d2cdf319b8add010
andrei_deshkevich <andrei.deshkevich@gmail.com>
2018-10-12 13:23:47 +0300

    auth fix

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('user: ' + req.user);
-  res.render('index', { title: 'Express', user:  req.user});
+  //console.log('user: ' + req.user);
+  //res.render('index', { title: 'Express', user:  req.user});
+    res.send('Hello World!');
 });
 
 module.exports = router;
