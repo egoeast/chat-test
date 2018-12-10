547b4372b2a1a903ff12988f8fcdd7f3d3a1c4d7
andrei_deshkevich <andrei.deshkevich@gmail.com>
2018-12-10 17:41:27 +0300

    db fix

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -4,15 +4,14 @@ const mongoose = require('mongoose');
 //mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'));
 
 
-mongoose.connect('mongodb://localhost/chat', {
+/*mongoose.connect('mongodb://localhost/chat', {
     "server": {
         "socetOptions": {
             "keepAlive": 1
         }
     }
-});
+});*/
 
-/*
 mongoose.connect('mongodb://user:pass123@ds263571.mlab.com:63571/egoeast-chat', {
     "server": {
         "socetOptions": {
@@ -20,7 +19,6 @@ mongoose.connect('mongodb://user:pass123@ds263571.mlab.com:63571/egoeast-chat', 
         }
     }
 });
-*/
 
 
 
